6a9ffc518ec8b1bd5518e407ce665f17e0b7417c
Billy Nicart <billy.nicart@gmail.com>
2017-06-10 13:01:56 +0800

    Ionic Crash Course Part 5: How to Store Data inside Ionic Apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HttpModule } from '@angular/http';
 import { ApiProvider } from '../providers/api/api';
 import { EmailComposer } from '@ionic-native/email-composer';
+import { FavoriteProvider } from '../providers/favorite/favorite';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { EmailComposer } from '@ionic-native/email-composer';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,6 +30,7 @@ import { EmailComposer } from '@ionic-native/email-composer';
     SplashScreen,
     ApiProvider,
     EmailComposer,
+    FavoriteProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
--- src/pages/film-details/film-details.ts
+++ src/pages/film-details/film-details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EmailComposer } from '@ionic-native/email-composer';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
 
 @IonicPage()
 @Component({
@@ -9,16 +10,33 @@ import { EmailComposer } from '@ionic-native/email-composer';
 })
 export class FilmDetailsPage {
   film: any;
+  isFavorite = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public favoriteProvider: FavoriteProvider,
     private emailComposer: EmailComposer) {
     this.film = this.navParams.get('film');
+    this.favoriteProvider.isFavorite(this.film.episode_id).then(isFav => {
+      this.isFavorite = isFav;
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FilmDetailsPage');
+  }
+
+  favoriteFilm() {
+    this.favoriteProvider.favoriteFilm(this.film.episode_id).then(() => {
+      this.isFavorite = true;
+    });
+  }
+
+  unfavoriteFilm() {
+    this.favoriteProvider.unfavoriteFilm(this.film.episode_id).then(() => {
+      this.isFavorite = false;
+    });
   }
 
   shareFilm() {
